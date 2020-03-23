1dcbd0d54a492781e94c36ed2dffeb67d639496e
cenzhilin <690693258@qq.com>
2020-03-23 22:57:07 +0800

    Updates

---
+++ precache-manifest.393495259f00a129849dae82943e5373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbf03f800c260bf8894138a6af4e69a",
+    "url": "/myweb/index.html"
+  },
+  {
+    "revision": "61bf09e2f1b3eb262555",
+    "url": "/myweb/static/css/main.c651c847.chunk.css"
+  },
+  {
+    "revision": "5f6852a94413e13d056f",
+    "url": "/myweb/static/js/2.f597a403.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/myweb/static/js/2.f597a403.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61bf09e2f1b3eb262555",
+    "url": "/myweb/static/js/main.505ef758.chunk.js"
+  },
+  {
+    "revision": "991e372eec1d5bd4c9d2",
+    "url": "/myweb/static/js/runtime-main.b5d66a72.js"
+  }
+]);
